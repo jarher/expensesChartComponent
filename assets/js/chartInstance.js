7eec7357254b5b5fecf1313660d0a405446e3491
jarher <jefferh33@gmail.com>
2024-12-10 17:12:46 -0500

    mobile css styles finished: fixed chart responsiveand adding general styles

--- assets/js/chartInstance.js
+++ assets/js/chartInstance.js
@@ -22,6 +22,11 @@ const runChartTable = async () => {
 
   const ctx = document.getElementById("myChart");
 
+  // Make sure the canvas has a container with relative dimensions
+  ctx.parentElement.style.height = "40vh";
+  ctx.parentElement.style.width = "100%";
+  ctx.parentElement.style.position = "relative";
+
   new Chart(ctx, {
     type: "bar",
     data: {
@@ -33,12 +38,21 @@ const runChartTable = async () => {
           yAxisID: "yAxis",
           xAxisID: "xAxis",
           backgroundColor,
-          borderRadius: "10",
+          borderRadius: function (context) {
+            // adjust borderRadius according canvas width
+            const chart = context.chart;
+            const width = chart.width;
+            return width < 400 ? 5 : 10;
+          },
           hoverBackgroundColor,
+          barPercentage: 0.8, // controls width bars
+          categoryPercentage: 0.9, //controls space between bar groups
         },
       ],
     },
     options: {
+      responsive: true,
+      maintainAspectRatio: false,
       plugins: {
         legend: {
           display: false,
@@ -48,16 +62,25 @@ const runChartTable = async () => {
           color: "hsl(25, 47%, 15%)",
           text: "Spending - Last 7 days",
           align: "start",
-          font: { weight: "bold" },
-          padding: 50,
+          font: {
+            color: "hsl(25, 47%, 15%)",
+            family: "DM Sans Bold",
+            size: function (context) {
+              // adjust font size according canvas size
+              const width = context.chart.width;
+              return width <= 425 ? 23 : 24;
+            },
+          },
+          padding: {
+            top: 20,
+            bottom: 20,
+          },
         },
         tooltip: {
-          enabled: false, // Desactivamos el tooltip por defecto
+          enabled: false,
           external: function (context) {
-            // Obtener el elemento tooltip
             let tooltipEl = document.getElementById("chartjs-tooltip");
 
-            // Crear el elemento si no existe
             if (!tooltipEl) {
               tooltipEl = document.createElement("div");
               tooltipEl.id = "chartjs-tooltip";
@@ -65,22 +88,18 @@ const runChartTable = async () => {
               document.body.appendChild(tooltipEl);
             }
 
-            // Ocultar si no hay hover
             if (context.tooltip.opacity === 0) {
               tooltipEl.style.opacity = 0;
               return;
             }
 
-            // Establecer el contenido
             const tableRoot = tooltipEl.querySelector("table");
 
             if (context.tooltip.body) {
               const bodyLines = context.tooltip.body.map((b) => b.lines);
-              let innerHtml = "<thead>";
+              let innerHtml = "<thead></thead><tbody>";
 
-              innerHtml += "</thead><tbody>";
-
-              bodyLines.forEach((body, i) => {
+              bodyLines.forEach((body) => {
                 innerHtml += `<tr><td>$${body}</td></tr>`;
               });
               innerHtml += "</tbody>";
@@ -88,30 +107,28 @@ const runChartTable = async () => {
               tableRoot.innerHTML = innerHtml;
             }
 
-            // Establecer estilos del tooltip
+            // tooltip responsive styles
+            const fontSize = context.chart.width < 400 ? "12px" : "14px";
+            tooltipEl.style.fontSize = fontSize;
             tooltipEl.style.opacity = 1;
             tooltipEl.style.position = "absolute";
             tooltipEl.style.background = "rgba(0, 0, 0, 0.8)";
             tooltipEl.style.color = "white";
-            tooltipEl.style.padding = "3px";
+            tooltipEl.style.padding = context.chart.width < 400 ? "2px" : "3px";
             tooltipEl.style.borderRadius = "4px";
             tooltipEl.style.pointerEvents = "none";
             tooltipEl.style.transform = "translate(-50%, 0)";
             tooltipEl.style.transition = "all .1s ease";
 
-            // Establecer la posición
             const position = context.chart.canvas.getBoundingClientRect();
-            const bodyPosition = document.body.getBoundingClientRect();
-
-            // Calcular la posición exacta
             const tooltipX =
               position.left + window.pageXOffset + context.tooltip.caretX;
             const tooltipY =
               position.top + window.pageYOffset + context.tooltip.caretY;
 
-            // Posicionar el tooltip 20px arriba de la barra
             tooltipEl.style.left = tooltipX + "px";
-            tooltipEl.style.top = tooltipY - 40 + "px";
+            tooltipEl.style.top =
+              tooltipY - (context.chart.width < 400 ? 30 : 40) + "px";
           },
         },
       },
@@ -131,6 +148,15 @@ const runChartTable = async () => {
           },
           border: {
             display: false,
+          },
+          ticks: {
+            font: {
+              color: "hsl(28, 10%, 53%)",
+              size: function (context) {
+                // adjust font size form X axis according its width
+                return context.chart.width < 400 ? 10 : 12;
+              },
+            },
           },
         },
       },
